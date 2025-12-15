3aa93e36eb2638abedb8d1c84f3b534a5c3ced96
Patryk Lewandowski <patryk.lewandowski@zeiss.com>
2025-12-15 08:46:14 +0100

    fix: fix angular monorepo resolution

--- lua/plugins/lsp/angular.lua
+++ lua/plugins/lsp/angular.lua
@@ -1,26 +1,34 @@
--- return {
---   'neovim/nvim-lspconfig',
---   opts = {
---     servers = {
---       angularls = {
---         cmd = {
---           'ngserver',
---           '--stdio',
---           '--tsProbeLocations', vim.fn.getcwd() .. '/node_modules',
---           '--ngProbeLocations', vim.fn.expand('~/.nvm/versions/node/v22.16.0/lib/node_modules/@angular/language-server/node_modules')
---         },
---         on_attach = function(client, bufnr)
---           require('lspconfig').common_on_attach(client, bufnr)
---         end,
---         filetypes = { 'html', 'typescript', 'javascript' },
---         root_dir = require('lspconfig/util').root_pattern(
---           'angular.json',
---           '.git'
---         ),
---         settings = {
---           angular = {},
---         },
---       },
---     },
---   },
--- }
+return {
+  'neovim/nvim-lspconfig',
+  opts = {
+    servers = {
+      angularls = {
+        cmd = {
+          'ngserver',
+          '--stdio',
+          '--tsProbeLocations',
+          vim.fn.getcwd() .. '/node_modules',
+          '--ngProbeLocations',
+          vim.fn.expand '~/.nvm/versions/node/v24.11.1/lib/node_modules/@angular/language-server/node_modules',
+        },
+        on_attach = function(client, bufnr)
+          require('lspconfig').common_on_attach(client, bufnr)
+        end,
+        filetypes = { 'html', 'typescript', 'javascript' },
+        root_dir = require('lspconfig/util').root_pattern '.git',
+        settings = {
+          angular = {},
+        },
+      },
+      vtsls = {
+        root_dir = require('lspconfig/util').root_pattern '.git',
+      },
+      ts_ls = {
+        root_dir = require('lspconfig/util').root_pattern '.git',
+      },
+      eslint = {
+        root_dir = require('lspconfig/util').root_pattern '.git',
+      },
+    },
+  },
+}
