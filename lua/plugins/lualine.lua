306fb76739992f17d1d22dd71d24aeb91643f9f6
Patryk Lewandowski <patryk.lewandowski@zeiss.com>
2026-03-27 07:28:48 +0100

    feat: remove clock from neovim

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,13 +3,14 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'Avi-D-coder/whisper.nvim' },
     opts = function(_, opts)
+      opts.sections = opts.sections or {}
+      opts.sections.lualine_x = opts.sections.lualine_x or {}
+      opts.sections.lualine_z = {}
+
       local ok, whisper = pcall(require, 'whisper')
       if not ok then
         return
       end
-
-      opts.sections = opts.sections or {}
-      opts.sections.lualine_x = opts.sections.lualine_x or {}
 
       table.insert(opts.sections.lualine_x, 1, whisper.lualine_component)
     end,
